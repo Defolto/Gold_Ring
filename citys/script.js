d0d52981b45869ca25895d76c09e78cff0bb2854
Максим <defoltmaks@gmail.com>
2019-05-17 21:54:33 +0300

    End Site

---
+++ citys/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    if(window.innerWidth < 850){
+        $(".block").removeClass("mx-5");
+        $(".block").addClass("mx-2");
+        $(".block").removeClass("px-5");
+        $(".block").addClass("px-3");
+    }
+});
