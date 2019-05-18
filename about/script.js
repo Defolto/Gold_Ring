a5d3edbd43a9f10cb0e59983d2365d3444701a1b
Максим <defoltmaks@gmail.com>
2019-05-18 12:35:58 +0300

    end site 2.0

---
+++ about/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    if(window.innerWidth < 960){
+        $("#short").removeClass("mx-5");
+        $("#short").addClass("mx-2");
+    }
+    if(window.innerWidth < 800){
+        $("#map").empty();
+        $("#map").append('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Afe4da413e83a5e32e20bb6ec66d2b4acddb92cbcaed871a8fa883c3c8c3464e1&amp;width=320&amp;height=240&amp;lang=ru_RU&amp;scroll=true"></script>');
+        $("#contact").removeClass("px-5");
+        $("#contact").addClass("px-3");
+    }
+});
